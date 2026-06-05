2156d1076612d7f681f96d7e12b80f0953864dab
naTan-Sa <natansahlu15@gmail.com>
2026-06-05 14:01:34 +0000

    refactor(task0): update _puts signature to accept va_list

--- _puts.c
+++ _puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout and counts the characters.
- * @str: Pointer to the string.
+ * _puts - Extracts and prints a string from the variadic arguments.
+ * @args: The va_list containing the string argument.
  *
  * Return: Number of characters printed.
  */
-int _puts(char *str)
+int _puts(va_list args)
 {
+	char *str = va_arg(args, char *);
 	int count = 0;
 
 	if (str == NULL)
