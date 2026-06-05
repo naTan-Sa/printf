d3b359047352c0f8397d9b11d9af4d189e8c6aa0
naTan-Sa <natansahlu15@gmail.com>
2026-06-05 21:44:05 +0000

    build: clean up prototypes in main.h after base engine consolidation

--- main.h
+++ main.h
@@ -17,10 +17,19 @@ int _print_int(va_list args);
 int _print_char(va_list args);
 int _puts(va_list args);
 int check_spec(char format_char, va_list args);
-int _print_binary(va_list args);
+
 
 /* Internal Calculation Helper */
 int int_recursion(unsigned int num);
-int binary_recursion(unsigned int num);
+
+/* Task 3 & 2 Unsigned Base Blueprints */
+int base_recursion(unsigned int num, int base, int upper);
+int _print_unsigned(va_list args);
+int _print_octal(va_list args);
+int _print_hex_lower(va_list args);
+int _print_hex_upper(va_list args);
+int _print_binary(va_list args);
+
+#endif /* MAIN_H */
 
 #endif
