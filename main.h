09ffaddaa75385e8ad5ab1cf8ed621064e97300d
naTan-Sa <natansahlu15@gmail.com>
2026-06-05 21:51:55 +0000

    build: clean up prototypes in main.h after base engine consolidation

--- main.h
+++ main.h
@@ -11,12 +11,18 @@ typedef struct specifier
 	int (*f)(va_list);
 } spec_t;
 
+/* Standard Low-Level Output */
 int _putchar(char c);
+
+/* Main Printf Engine & Selection Switchboard */
 int _printf(const char *format, ...);
+int check_spec(char format_char, va_list args);
+
+/* Core Variadic Specifier Handlers */
 int _print_int(va_list args);
 int _print_char(va_list args);
 int _puts(va_list args);
-int check_spec(char format_char, va_list args);
+
 
 
 /* Internal Calculation Helper */
@@ -31,5 +37,3 @@ int _print_hex_upper(va_list args);
 int _print_binary(va_list args);
 
 #endif /* MAIN_H */
-
-#endif
