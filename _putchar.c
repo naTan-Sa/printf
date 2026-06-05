0dcf3355374b8c92be4aaf76984c310351f9de8f
naTan-Sa <natansahlu15@gmail.com>
2026-06-05 22:10:38 +0000

    perf: refactor _putchar to collect characters in a 1024-byte static buffer

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to be printed
+ * _putchar - Buffers a character and writes to stdout when full or flushed.
+ * @c: The character to print. Pass -1 to force a buffer flush.
  *
- * Return: On success 1.
+ * Return: Always returns 1.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		if (i > 0)
+		{
+			write(1, buffer, i);
+			i = 0;
+		}
+	}
+
+	if (c != -1)
+	{
+		buffer[i] = c;
+		i++;
+	}
+	return (1);
 }
